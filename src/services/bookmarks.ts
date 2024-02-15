c375732726ddafa0054e22b6047a8e028ac06e7f
Guilherme S <guinho.shuto@gmail.com>
2024-02-14 22:45:03 -0400

    ajustes no puppeteer

--- src/services/bookmarks.ts
+++ src/services/bookmarks.ts
@@ -30,6 +30,7 @@ export default class Bookmarks{
         try{
             const browser = await puppeteer.launch({
                 headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
             }) 
             const page = await browser.newPage()
             await page.goto(url, {
